13d2af51091a2cadd56ff24f315c669d3c07ec52
vimofama <vimofama@outlook.com>
2023-09-04 09:59:17 -0500

    Validar entradas

--- frontend/js/reseteo.js
+++ frontend/js/reseteo.js
@@ -1,13 +1,14 @@
 // Initialize Firebase
 var firebaseConfig = {
-  // Aquí debes agregar la configuración de tu proyecto Firebase
-  apiKey: "AIzaSyAq3PWrGxMVqAcvfXwpC21vzsSQlVI-AdM",
-  authDomain: "polimarket-93916.firebaseapp.com",
-  projectId: "polimarket-93916",
-  storageBucket: "polimarket-93916.appspot.com",
-  messagingSenderId: "624045411960",
-  appId: "1:624045411960:web:55537b983ead1f3516e78d",
+    // Aquí debes agregar la configuración de tu proyecto Firebase
+    apiKey: "AIzaSyAq3PWrGxMVqAcvfXwpC21vzsSQlVI-AdM",
+    authDomain: "polimarket-93916.firebaseapp.com",
+    projectId: "polimarket-93916",
+    storageBucket: "polimarket-93916.appspot.com",
+    messagingSenderId: "624045411960",
+    appId: "1:624045411960:web:55537b983ead1f3516e78d",
 };
+
 firebase.initializeApp(firebaseConfig);
 
 // Obtener referencias a los elementos del formulario
@@ -17,7 +18,15 @@ var emailInput = document.getElementById("email");
 document.getElementById("formulario").addEventListener("submit", function (e) {
   e.preventDefault(); // Evitar el envío del formulario por defecto
 
-  var email = emailInput.value;
+  var email = emailInput.value.trim();
+
+  // Validar que el correo tenga un formato válido
+  var emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+
+  if (!emailRegex.test(email)) {
+    alert("Por favor, ingresa un correo electrónico válido.");
+    return; // No continuar si el correo no es válido
+  }
 
   // Llama a la función de Firebase para restablecer la contraseña
   firebase
